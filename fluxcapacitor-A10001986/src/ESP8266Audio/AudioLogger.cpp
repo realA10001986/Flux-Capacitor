017d39dd84ec6cd46aeb6c10cd03ae4df179f66c
A10001986 <76924199+realA10001986@users.noreply.github.com>
2026-06-24 07:57:25 +0200

    Audio patches for 1.104

--- fluxcapacitor-A10001986/src/ESP8266Audio/AudioLogger.cpp
+++ fluxcapacitor-A10001986/src/ESP8266Audio/AudioLogger.cpp
@@ -1,5 +1,9 @@
 
 #include "AudioLogger.h"
 
+#ifdef HAVE_AUDIO_LOGGER
+
 DevNullOut silencedLogger;
 Print* audioLogger = &silencedLogger;
+
+#endif
---
+++ fluxcapacitor-A10001986/src/ESP8266Audio/AudioLoggerLocal.h
@@ -0,0 +1,11 @@
+
+#ifndef _AUDIO_LOGGER_LOCAL
+#define _AUDIO_LOGGER_LOCAL
+
+#include "../../fc_global.h"
+
+#ifdef FC_DBG
+#define HAVE_AUDIO_LOGGER
+#endif
+
+#endif
